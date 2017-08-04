2fc503e4b63fc23e67580596720741f1300e2f4e
Marcelo Fernandes <marcelo.fernandes@sou2adv.in>
2017-08-03 23:38:06 -0300

    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-interakt',
   
   contentFor: function(type, config) {
-   let appId = config['ember-cli-interakt'].appId,
+   let appId = config['ember-cli-interakt'].appId;
     if (type === 'head') {
       return '<script>(function() {var interakt = document.createElement("script"); interakt.type = "text/javascript"; interakt.async = true; interakt.src = "//cdn.interakt.co/interakt/'+appId+'.js";var scrpt = document.getElementsByTagName("script")[0]; scrpt.parentNode.insertBefore(interakt, scrpt);})()</script>';
     }
